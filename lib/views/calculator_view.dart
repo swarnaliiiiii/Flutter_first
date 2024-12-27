0077be470c85a3d85902be6b7b1b801174c19acf
Swarnali Bhattacharyya <oli2k3@gmail.com>
2024-12-27 12:32:13 +0530

    third

--- lib/views/calculator_view.dart
+++ lib/views/calculator_view.dart
@@ -1,24 +1,118 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class CalculatorView extends StatelessWidget {
+class CalculatorView extends StatefulWidget {
   const CalculatorView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        // Calculator display
-        TextField(
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: "0",
-            hintText: "Enter a number",
-          ),
-        )
-        //Expand
-        //Calculator buttons
+  State<CalculatorView> createState() => _CalculatorViewState();
+}
 
-      ],
+class _CalculatorViewState extends State<CalculatorView> {
+  int x = 0;
+  int y = 0;
+  num z = 0;
+
+  final displayOneController = TextEditingController();
+  final displayTwoController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    displayOneController.text = x.toString();
+    displayTwoController.text = y.toString();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: Column(
+        children: [
+          // Calculator display
+          DisplayOne(
+              hint: "Enter first number", controller: displayOneController),
+          SizedBox(
+            height: 30,
+          ),
+          DisplayOne(
+              hint: "Enter second number", controller: displayTwoController),
+          SizedBox(
+            height: 30,
+          ),
+          Text(z.toString(),
+              style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold)),
+          Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              FloatingActionButton(
+                onPressed: () {
+                  z = (num.tryParse(displayOneController.text) ?? 0) +
+                      (num.tryParse(displayTwoController.text) ?? 0);
+                  print(z);
+                },
+                child: const Icon(CupertinoIcons.add),
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    z = num.tryParse(displayOneController.text)! -
+                        num.tryParse(displayTwoController.text)!;
+                    print(z);
+                  });
+                },
+                child: const Icon(CupertinoIcons.minus),
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  x = int.parse(displayOneController.text);
+                  y = int.parse(displayTwoController.text);
+                  z = x * y;
+                },
+                child: const Icon(CupertinoIcons.multiply),
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  z = x / y;
+                },
+                child: const Icon(CupertinoIcons.divide),
+              ),
+            ],
+          )
+
+          //Expand
+          //Calculator buttons
+        ],
+      ),
+    );
+  }
+}
+
+class DisplayOne extends StatelessWidget {
+  const DisplayOne({
+    super.key,
+    this.hint = "Enter a number",
+    required this.controller,
+  });
+
+  final String? hint;
+  final TextEditingController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      keyboardType: TextInputType.number,
+      autofocus: true,
+      decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.black, width: 3),
+              borderRadius: BorderRadius.circular(10)),
+          border: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.black, width: 3),
+              borderRadius: BorderRadius.circular(10)),
+          hintText: hint,
+          hintStyle: TextStyle(color: Colors.white)),
     );
   }
 }
